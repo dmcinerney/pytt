5aebe7137b09d9ca38748553e0e4509498a2a795
dmcinerney <dmciner1@jhu.edu>
2019-08-09 15:16:47 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pytt',
+    version='0.1',
+    description='Pytorch Training and Testing, a module containing advanced and organized batching, training, and testing with multi-gpu and delayed update support',
+    url='https://github.com/dmcinerney/pytt',
+    author='Denis Jered McInerney',
+    author_email='jered.mcinerney@gmail.com',
+#    license='',
+    packages=['pytt'],
+#    zip_safe=False
+)
