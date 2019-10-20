5db30aed7c044d1a5f1ff3ad2b8dc1d5d3f911b1
Jered McInerney <jered.mcinerney@gmail.com>
2019-10-19 22:37:06 -0400

    bug fix in trainer to have input tracker

--- pytt/training/trainer.py
+++ pytt/training/trainer.py
@@ -36,7 +36,8 @@ class Trainer:
         self.train_iterator = train_iterator
         self.pbar = pbar if pbar is not None else ProgressBar()
         self.val_iterator = val_iterator
-        self.tracker = Tracker(checkpoint_folder=checkpoint_folder)
+        self.tracker = Tracker(checkpoint_folder=checkpoint_folder)\
+                       if tracker is None else Tracker
         self.checkpoint_folder = checkpoint_folder
         self.batch_info_class = batch_info_class
         self.val_every = val_every
